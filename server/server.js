6cc50bcc5c486bc73c3da0eb812ec72a4b41c82f
Gina <wlskb@naver.com>
2023-11-30 22:45:01 +0900

    [BE] Feat : 라우터 개발_ upload post 요청 개발

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const express=require('express') //express를 불러옴
+
+const app=express(); //앱을 만들어줌
+const PORT=5000; //사용한 포트번호를 선언
+
+
+// 라우터 만들기
+app.post("/upload", (req, res)=>{
+    console.log("/upload called!");
+    res.json({result:"success"});
+})
+
+
+app.listen(PORT, ()=> console.log("Express server listening on PORT "+PORT)); //port연결
+
+
+
+
